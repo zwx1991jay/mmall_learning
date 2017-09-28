4b5e91740b30b58017914799a40acd4cb85a96cc
zwx1991jay <371049909@qq.com>
2017-09-28 18:57:14 +0800

    5.3注册接口9.28

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -6,4 +6,10 @@ package com.mmall.common;
  */
 public class Const {
     public static final String CURRENT_USER = "current_user";
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+    public interface Role{
+        int ROLE_CUSTOMER = 0 ;//普通用户
+        int ROLE_ADMIN = 1;//管理员
+    }
 }
--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.mmall.service.impl;
 
+import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.MD5Util;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,12 +25,53 @@ public class UserServiceImpl implements IUserService {
         if(resultCount == 0){
             return ServerResponse.createByErrorMessage("用户名不存在");
         }
-        //登陆密码MD5
-        User user = userMapper.selectLogin(username,password);
+        //to do 登陆密码MD5
+        String md5Password = MD5Util.MD5EncodeUtf8(password);
+        User user = userMapper.selectLogin(username,md5Password);
         if(user == null){
             return ServerResponse.createByErrorMessage("密码错误");
         }
         user.setPassword(StringUtils.EMPTY);
         return ServerResponse.createBysuccess("登陆成功",user);
+    }
+
+    public ServerResponse<String> register(User user){
+        ServerResponse validResponse = this.checkValid(user.getUsername(),Const.USERNAME);
+        if(!validResponse.isSuccess()){
+            return validResponse;
+        }
+        validResponse = this.checkValid(user.getUsername(),Const.EMAIL);
+        if(!validResponse.isSuccess()){
+            return validResponse;
+        }
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        //MD5加密
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+        int resultCount = userMapper.insert(user);
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("注册失败");
+        }
+        return ServerResponse.createBysuccessMessage("注册成功");
+    }
+
+
+    public  ServerResponse<String> checkValid(String str,String type){
+        if(StringUtils.isNotBlank(type)){
+            if(Const.USERNAME.equals(type)){
+                int resultCount = userMapper.checkUsername(str);
+                if(resultCount > 0){
+                    return ServerResponse.createByErrorMessage("用户名已存在");
+                }
+            }
+            if(Const.EMAIL.equals(type)){
+                int resultCount = userMapper.checkEmail(str);
+                if(resultCount > 0){
+                    return ServerResponse.createByErrorMessage("邮箱已存在");
+                }
+            }
+        }else{
+            return ServerResponse.createByErrorMessage("参数错误");
+        }
+        return ServerResponse.createBysuccessMessage("校验成功");
     }
 }
